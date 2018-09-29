3a609e20e2a3fbbc1de321741dfc9af3c88f8198
Andy Bui <andyaaoux@hotmail.com>
2018-09-29 08:56:34 -0400

    Pie Chart

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -15,21 +15,39 @@ export default {
   },
   data () {
     return {
-      datacollection: {
-        labels: this.labels,
-        datasets: [
-          {
-            label: 'User Data',
-            backgroundColor: '#f87979',
-            data: this.data
-          }
-        ]
+    }
+  },
+  methods:{
+    getRandomColor:function() {
+      var letters = '0123456789ABCDEF';
+      var color = '#';
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
       }
+      return color;
     }
   },
   mounted () {
-  
-    this.renderChart(this.datacollection, {responsive: true, maintainAspectRatio: false})
+    var datasets = []
+    console.log(this.labels)
+    console.log(this.data)
+    for(var i = 0; i < this.labels.length ; i++){
+      datasets.push(
+        {
+          label: this.labels[i],
+          backgroundColor: this.getRandomColor(),
+          data: [this.data[i]]
+        }
+      )
+      
+    }
+    console.log(datasets)
+    this.renderChart(
+      {
+        labels: this.labels,
+        datasets: datasets,
+        responsive: true, 
+        maintainAspectRatio: false})
+      }
   }
-}
 </script>
